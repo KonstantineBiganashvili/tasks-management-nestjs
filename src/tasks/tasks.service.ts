f0e09a019e32781fb6785d5db9d659ab88420d87
KonstantineBiganashvili <konstantinebiganashvili@gmail.com>
2023-05-31 11:13:18 +0400

    error handling

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -13,7 +13,12 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+    if (!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+
+    return found;
   }
 
   getTasksWithFilters(filterData: GetTasksFilterDto): Task[] {
@@ -51,9 +56,9 @@ export class TasksService {
   }
 
   deleteTaskById(id: string): Task {
-    const task = this.getTaskById(id);
+    const found = this.getTaskById(id);
     this.tasks = this.tasks.filter((task) => task.id !== id);
-    return task;
+    return found;
   }
 
   editTaskById(id: string, status: TaskStatus): Task {
