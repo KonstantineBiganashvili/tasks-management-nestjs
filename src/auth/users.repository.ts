c1a9a606e2b87b73c57f32796c2c106c0fa9bb36
KonstantineBiganashvili <konstantinebiganashvili@gmail.com>
2023-05-31 19:15:51 +0400

    basic sign up

---
+++ src/auth/users.repository.ts
@@ -0,0 +1,30 @@
+import {
+  Injectable,
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+@Injectable()
+export class UsersRepository extends Repository<User> {
+  constructor(private dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    try {
+      const user: User = this.create({ username, password });
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException('Unexpected error');
+      }
+    }
+  }
+}
